928d7d1c48291ae3a70db7d348b6e79edabc39a1
Ben Sweetman <ben.sweetman@savient.uk.com>
2026-01-29 22:08:03 +0000

    have to signup

--- cypress/tests/ui/messing-around.spec.ts
+++ cypress/tests/ui/messing-around.spec.ts
@@ -26,9 +26,29 @@ describe("testing various bits", function () {
   });
 
   it("should prompt to create a bank account on first login", function () {
-    cy.login('existinguser', 's3cret');
+    cy.visit("/");
 
-    cy.getBySel('user-onboarding-dialog').should('be.visible');
-    cy.getBySel('bankaccount-submit').should('be.disabled');
+    cy.getBySel('signup').click();
+    cy.getBySel('signup-title').should('be.visible').and('contain', 'Sign Up');
+    cy.url().should('include', '/signup');
+    cy.getBySel('signup-submit').click();
+    cy.getBySel('signup-submit').should('be.disabled');
+  
+    cy.getBySel('signup-first-name').type('Test');
+    cy.getBySel('signup-last-name').type('User');
+    cy.getBySel('signup-username').type('testuser123');
+    cy.getBySel('signup-password').type('s3cret');
+    cy.getBySel('signup-confirmPassword').type('s3cret');
+
+    cy.getBySel('signup-submit').should('be.enabled');
+
+    cy.intercept('POST', '/users').as('signupRequest');
+    cy.getBySel('signup-submit').click();
+
+    cy.login('testuser123', 's3cret');
+
+    cy.getBySel('user-onboarding-dialog').should('be.visible').and('contain', 'Get Started with Real World App');
+    cy.getBySel('user-onboarding-next').click();
+    
   });
 });
