e65b04e334c65b6130374850cc0d1e8c442fd55a
Ben Sweetman <ben.sweetman@savient.uk.com>
2026-01-30 16:26:59 +0000

    made onboarding test work

--- cypress/tests/ui/messing-around.spec.ts
+++ cypress/tests/ui/messing-around.spec.ts
@@ -49,6 +49,34 @@ describe("testing various bits", function () {
 
     cy.getBySel('user-onboarding-dialog').should('be.visible').and('contain', 'Get Started with Real World App');
     cy.getBySel('user-onboarding-next').click();
-    
+
+    cy.getBySel('bankaccount-bankName-input').click();
+    cy.getBySel('bankaccount-submit').click();
+    cy.getBySel('bankaccount-submit').should('be.disabled');
+
+    cy.getBySel('bankaccount-bankName-input').type('First Bank');
+    cy.getBySel('bankaccount-routingNumber-input').type('123456789');
+    cy.getBySel('bankaccount-accountNumber-input').type('987654321');
+
+    cy.getBySel('bankaccount-submit').should('be.enabled');
+
+    cy.intercept('POST', '/graphql').as('createBankAccount');
+    cy.getBySel('bankaccount-submit').click();
+    cy.wait('@createBankAccount').then((subject) => {
+      const response = subject.response;
+      expect(response?.statusCode).to.eq(200);
+      const createRequest = subject.request.body;
+      expect(createRequest).to.have.property('operationName', 'CreateBankAccount');
+      expect(createRequest.variables.bankName).to.equal('First Bank');
+      expect(createRequest.variables.routingNumber).to.equal('123456789');
+      expect(createRequest.variables.accountNumber).to.equal('987654321');
+      expect(createRequest.variables.userId).to.exist;
+    });
+
+    cy.getBySel('user-onboarding-dialog-title').should('contain', 'Finished');
+    cy.getBySel('user-onboarding-next').should('have.text', 'Done');
+    cy.getBySel('user-onboarding-next').click();
+  
+    cy.getBySel('user-onboarding-dialog').should('not.exist');
   });
 });
